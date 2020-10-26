7934afd83104cb4a1e4b4392c5c0687be11947d1
shubhanshdj <71318599+shubhanshdj@users.noreply.github.com>
2020-10-26 22:17:28 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -64,7 +64,7 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await event.edit(
-        f"__**✦҈͜͡➳ █▀▄   ▄▀▄    █▄   █    ▄▀      █  ██▀      ▀▄▀    █   ▀█    ▀▄█   ▄  ▄!__**\n★ {ms}\n★ __**Mʏ**__ __**Bᴏss**__ [{DEFAULTUSER}]"
+        f"__**✦҈͜͡➳ █▀▄   ▄▀▄    █▄   █    ▄▀      █  ██▀      ▀▄▀    █   ▀█    ▀▄█   ▄  ▄__**\n★ {ms}\n★ __**M̵͖̠͓̾̈́͝Ÿ̵̞̠͇́̔̓**__ __**M̸̦̙̘͊̈́A̴̦̞̝͒̓̓S̸͓͇͎͛͑͠T̴͚͖̈́̾̈́E̵͉̙̫̒̓̚R̵̡͔͓̔͛͘**__ [{DEFAULTUSER}]"
     )
 
 
